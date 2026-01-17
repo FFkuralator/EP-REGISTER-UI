37dc63556976774275e0b079df7d2d074c19b268
FFkuralator <95739275+FFkuralator@users.noreply.github.com>
2026-01-18 00:55:58 +1000

    mini fix PaginationBlock

--- src/components/UI/Table/PaginationBlock.jsx
+++ src/components/UI/Table/PaginationBlock.jsx
@@ -4,20 +4,6 @@ import styles from './PaginationBlock.module.css';
 export default function PaginationBlock({ onPageSizeChange, pageSizes, paginationState, onPageChange }) {
   return (
     <div className={styles.paginationWrapper}>
-      <div className={styles.topRow}>
-        <div className={styles.leftControls}>
-            {pageSizes ? (
-              pageSizes.map((size) => <option key={size} value={size}>{size}</option>)
-            ) : (
-              <>
-                <option value="20">20</option>
-                <option value="50">50</option>
-                <option value="100">100</option>
-              </>
-            )}
-        </div>
-      </div>
-
       <div className={styles.infoRow}>
         {paginationState.startItem}-{paginationState.endItem} из {paginationState.totalItems}
       </div>
