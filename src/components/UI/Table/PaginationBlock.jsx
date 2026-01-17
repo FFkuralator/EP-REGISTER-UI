03506c08a6bafe85981e364bdbdd2763f5d3d0be
FFkuralator <95739275+FFkuralator@users.noreply.github.com>
2026-01-18 00:45:19 +1000

    fix(pagination): remove navigation buttons and center page-size

--- src/components/UI/Table/PaginationBlock.jsx
+++ src/components/UI/Table/PaginationBlock.jsx
@@ -6,13 +6,6 @@ export default function PaginationBlock({ onPageSizeChange, pageSizes, paginatio
     <div className={styles.paginationWrapper}>
       <div className={styles.topRow}>
         <div className={styles.leftControls}>
-          <label>На странице: </label>
-          <select
-            name="pageSize"
-            className={styles.paginationSelect}
-            onChange={(e) => onPageSizeChange(e.target.value)}
-            value={paginationState.pageSize || (pageSizes ? pageSizes[0] : 20)}
-          >
             {pageSizes ? (
               pageSizes.map((size) => <option key={size} value={size}>{size}</option>)
             ) : (
@@ -22,27 +15,6 @@ export default function PaginationBlock({ onPageSizeChange, pageSizes, paginatio
                 <option value="100">100</option>
               </>
             )}
-          </select>
-        </div>
-
-        <div className={styles.rightControls}>
-          <button
-            type="button"
-            className={styles.paginationButton}
-            disabled={paginationState.currentPage === 1}
-            onClick={() => onPageChange(-1)}
-          >
-            ←
-          </button>
-          <span className={styles.pageState}>{paginationState.currentPage}/{paginationState.totalPages}</span>
-          <button
-            type="button"
-            className={styles.paginationButton}
-            disabled={paginationState.currentPage === paginationState.totalPages}
-            onClick={() => onPageChange(1)}
-          >
-            →
-          </button>
         </div>
       </div>
 
